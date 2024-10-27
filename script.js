c68dcd9fd67077fc848311f96467a6517c1a36ec
Mahakprajapati <mahakprajapati227@gmail.com>
2024-10-27 16:13:22 +0530

    third commit

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const button=document.querySelector('.button');
 const input=document.querySelector('input');
 const input_container=document.querySelector('.input-container');
-console.log(input);
+// console.log(input);
+
+let emailPattern=/^[A-Za-z._]{4,}[0-9]{0,3}@[gmail.com]{9}$/ ;
 
 button.addEventListener('click',()=>{
     let inputValue=input.value;
     // console.log(inputValue);
-    if(inputValue){
+    if(emailPattern.test(input.value)){
         input_container.classList.remove('errors');
-        alert(`you will get updation through your email`);
+        alert(`you will get updation through your email ${inputValue}`);
     }else{
         input_container.classList.add('errors');
     }
